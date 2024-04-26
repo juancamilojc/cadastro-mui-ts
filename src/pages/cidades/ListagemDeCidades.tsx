105df63efcef4cdcc0640883efa14471cbfb6c71
Juan Camilo <jcamilo596@gmail.com>
2024-04-26 16:06:45 -0300

    Criada a página de listagem de cidades

---
+++ src/pages/cidades/ListagemDeCidades.tsx
@@ -0,0 +1,28 @@
+import { useMemo } from 'react';
+import { useSearchParams } from 'react-router-dom';
+import { FerramentasDaListagem } from '../../shared/components';
+import { LayoutBaseDePagina } from '../../shared/layouts';
+
+export const ListagemDeCidades: React.FC = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const busca = useMemo(() => {
+    return searchParams.get('busca') || '';
+  }, [searchParams]);
+
+  return (
+    <LayoutBaseDePagina
+      titulo="Listagem de Cidades"
+      barraDeFerramentas={
+        <FerramentasDaListagem
+          mostrarInputBusca
+          textoBotaoNovo="Nova"
+          textoDaBusca={busca}
+          aoMudarTextoDeBusca={(texto) => setSearchParams({ busca: texto }, { replace: true })}
+        />
+      }
+    >
+      <div>Teste</div>
+    </LayoutBaseDePagina>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
-import { Dashboard } from '../pages';
+import { Dashboard, ListagemDeCidades } from '../pages';
 import { useDrawerContext } from '../shared/contexts';
 
 export const AppRoutes = () => {
@@ -14,12 +14,20 @@ export const AppRoutes = () => {
         label: 'Página Inicial',
         path: '/pagina-inicial',
       },
+      {
+        icon: 'location_city',
+        label: 'Cidades',
+        path: '/cidades',
+      },
     ]);
   }, []);
 
   return (
     <Routes>
       <Route path="/pagina-inicial" element={<Dashboard />} />
+
+      <Route path="/cidades" element={<ListagemDeCidades />} />
+      {/* <Route path="/cidades/detalhe/:id" element={<Dashboard />} /> */}
 
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
